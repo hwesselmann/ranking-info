ab7b201de1898781499d3febb0e3199e9ebef888
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-02 15:46:33 +0200

    add some static pages and configure guard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'import#info'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/about'
   get 'import/info'
   get 'import/import'
   resources :import do
