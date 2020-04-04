ec07c019439e522f94fc40658aac141c0e3282e3
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-04 22:03:58 +0200

    add more info and styling to status page

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -5,10 +5,42 @@ class ImportController < ApplicationController
 
   def info
     # show some stats
-    @player_count = Player.count
+    @player_male_count = Player.where("dtb_id LIKE '1%'").count
+    @player_female_count = Player.where("dtb_id LIKE '2%'").count
     @ranking_count = Ranking.count
     # available rankings
+    available_rankings = Ranking.select(:date).order(date: :desc).distinct
+    year = 0
+    years = {}
+    quarters = []
+    available_rankings.each do |ar|
+      unless ar.date.year.eql?(year)
+        # new year, shift
+        unless year.eql?(0)
+          years[year.to_s] = quarters.reverse
+          quarters.clear
+        end
+        year = ar.date.year
+      end
+      quarters.push ar.date.strftime('%d.%m.')
+    end
+    years[year.to_s] = quarters.reverse
+    @available_quarters = years
+
     # last import
+    last_updated = Ranking.order(updated_at: :desc).first
+    if last_updated.nil?
+      last_updated = ''
+    else
+      # convert to CEST time
+      last_updated = last_updated.updated_at.localtime('+02:00')
+      last_updated = last_updated.strftime('%d.%m.%Y %H:%M')
+    end
+    @date_last_updated = last_updated
+  end
+
+  def upload
+    # action is just for page navigation
   end
 
   def import
@@ -18,9 +50,9 @@ class ImportController < ApplicationController
         file.write(uploaded_file.read)
       end
       Player.import_rankings('public/uploads/' + uploaded_file.original_filename)
-      redirect(root_url, 'new rankings imported!')
+      redirect(status_url, 'new rankings imported!')
     else
-      redirect(root_url, "please upload a ranking file")
+      redirect(status_url, "please upload a ranking file")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/status', to: 'import#info'
+  get '/import', to: 'import#upload'
   post '/import', to: 'import#import'
   get '/players', to: 'players#index'
   get '/player/:id', to: 'players#show', as: :player
