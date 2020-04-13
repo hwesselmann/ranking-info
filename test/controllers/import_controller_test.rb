b60b5baa2ab5807eed5a350bf49285dcce268a1e
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-13 17:41:18 +0200

    add authorization

--- test/controllers/import_controller_test.rb
+++ test/controllers/import_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class ImportControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:testuser)
+  end
+
   test 'should get status' do
     get status_path
     assert_response :success
     assert_select 'title', full_title('Status')
+  end
+
+  test 'should redirect update when not logged in' do
+    get import_path
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
