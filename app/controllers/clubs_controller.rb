90af9a399b8ef9810f8983eb6455aee159f723c6
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-20 20:03:08 +0200

    refactor club controller to have smaller methods

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -7,20 +7,10 @@ class ClubsController < ApplicationController
   def index
     clubs = []
     if params[:commit]
-      current_quarter = Ranking.select(:date).order(date: :desc)
-                               .distinct.first.date
-      player_clubs = Player.select(:club)
-                           .where("dtb_id IN (SELECT DISTINCT(dtb_id) FROM rankings 
-                                   WHERE date='#{current_quarter}') 
-                                   AND club LIKE '%#{params[:club]}%'")
-                           .order(:club)
-                           .distinct
+      quarter = current_quarter
+      player_clubs = find_all_clubs(quarter, params[:club])
       player_clubs.each do |c|
-        player_count = Player.select(:club)
-                             .where("dtb_id IN (SELECT DISTINCT(dtb_id)
-                                     FROM rankings 
-                                     WHERE date='#{current_quarter}')
-                                     AND club='#{c.club}'").count
+        player_count = count_players_for_club(quarter, c.club)
         clubs.push({ name: c.club, player_count: player_count })
       end
     end
@@ -33,6 +23,37 @@ class ClubsController < ApplicationController
     players = Player.where("dtb_id IN (SELECT DISTINCT(dtb_id) FROM rankings
                             WHERE date='#{current_quarter}')
                             AND club='#{params[:id]}'").order(:lastname)
+    player_ranking = fill_club_info(players)
+    @players = player_ranking
+  end
+
+  private
+
+  def current_quarter
+    Ranking.select(:date)
+           .order(date: :desc)
+           .distinct
+           .first
+           .date
+  end
+
+  def find_all_clubs(quarter, club)
+    Player.select(:club)
+          .where("dtb_id IN (SELECT DISTINCT(dtb_id)
+                                FROM rankings WHERE date='#{quarter}')
+                                AND club LIKE '%#{club}%'")
+          .order(:club)
+          .distinct
+  end
+
+  def count_players_for_club(quarter, club)
+    Player.select(:club)
+          .where("dtb_id IN (SELECT DISTINCT(dtb_id) FROM rankings
+                  WHERE date='#{quarter}') AND club='#{club}'")
+          .count
+  end
+
+  def fill_club_info(players)
     player_ranking = []
     players.each do |player|
       ranking = Ranking.find_by(dtb_id: player.dtb_id,
@@ -42,6 +63,6 @@ class ClubsController < ApplicationController
                             firstname: player.firstname, score: ranking.score,
                             rank: ranking.ranking_position })
     end
-    @players = player_ranking
+    player_ranking
   end
 end
--- test/controllers/clubs_controller_test.rb
+++ test/controllers/clubs_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ClubsControllerTest < ActionDispatch::IntegrationTest
@@ -5,5 +7,11 @@ class ClubsControllerTest < ActionDispatch::IntegrationTest
     get clubs_path
     assert_response :success
     assert_select 'title', full_title('Vereinssuche')
+  end
+
+  test 'get club info' do
+    get club_path('Eintracht Frankfurt')
+    assert_response :success
+    assert_select 'title', full_title('Eintracht Frankfurt')
   end
 end
