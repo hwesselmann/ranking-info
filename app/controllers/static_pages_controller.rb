80bb19d0b4756c871ef6fbabb4bba3772bd68a08
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-21 23:57:32 +0200

    show first imported quarter on start page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,14 @@
 #
 class StaticPagesController < ApplicationController
   def home
-    # TODO: add date of first imported ranking or a default for use in view
+    first_import = Ranking.select(:date)
+                          .order(date: :desc)
+                          .distinct
+                          .last
+                          .date
+    @start = if first_import.nil? then 'xx.xx.xxxx'
+             else first_import.strftime('%d.%m.%Y')
+             end
   end
 
   def about; end
