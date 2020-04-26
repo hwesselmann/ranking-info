f0271c0f109a3ebb3da17f7133de404b0078c84b
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-26 12:02:21 +0200

    remove login link from start page

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -54,7 +54,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
-    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path, count: 0
   end
 end
