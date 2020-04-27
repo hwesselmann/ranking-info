adc010e167db83115d88b2471ec082a99c25d36f
Hauke Wesselmann <hauke@h-dawg.de>
2020-04-27 22:49:59 +0200

    refactorings and rubocop stuff

--- app/controllers/federations_controller.rb
+++ app/controllers/federations_controller.rb
@@ -1,30 +1,17 @@
+# frozen_string_literal: true
+
+#
+# Controller for federation data view.
+#
 class FederationsController < ApplicationController
   def index
     @federations = {}
-    quarter = Ranking.select(:date).order(date: :desc)
-                     .distinct.first
+    quarter = current_quarter
     return @federations if quarter.nil?
 
     quarter = quarter.date
-    sql = "SELECT COUNT(dtb_id) AS count, federation, age_group FROM rankings
-           WHERE date='#{quarter}'
-           AND dtb_id >= 10000000
-           AND dtb_id < 20000000
-           AND yob_ranking=false
-           AND age_group_ranking=true
-           AND year_end_ranking=false
-           GROUP BY federation, age_group;"
-    male_count = ActiveRecord::Base.connection.exec_query(sql)
-
-    sql = "SELECT COUNT(dtb_id) AS count, federation, age_group FROM rankings
-           WHERE date='#{quarter}'
-           AND dtb_id >= 20000000
-           AND dtb_id < 30000000
-           AND yob_ranking=false
-           AND age_group_ranking=true
-           AND year_end_ranking=false
-           GROUP BY federation, age_group;"
-    female_count = ActiveRecord::Base.connection.exec_query(sql)
+    male_count = player_count_by_federation(quarter, 'm')
+    female_count = player_count_by_federation(quarter, 'w')
 
     federations = {}
     age_groups = {}
@@ -48,5 +35,23 @@ class FederationsController < ApplicationController
       federations[entry['federation']][age_group] = entry['count']
     end
     @federations = federations
+  end
+
+  def current_quarter
+    Ranking.select(:date).order(date: :desc)
+           .distinct.first
+  end
+
+  def player_count_by_federation(quarter, gender)
+    dtb_id = if gender.eql?('m') then 10_000_000
+             else 20_000_000
+             end
+    sql = "SELECT COUNT(dtb_id) AS count, federation, age_group FROM rankings
+           WHERE date='#{quarter}' AND dtb_id >= #{dtb_id}
+           AND dtb_id < #{dtb_id + 10_000_000}
+           AND yob_ranking=false AND age_group_ranking=true
+           AND year_end_ranking=false
+           GROUP BY federation, age_group;"
+    ActiveRecord::Base.connection.exec_query(sql)
   end
 end
