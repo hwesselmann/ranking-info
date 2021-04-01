873ee8d46447262329daa8f323bf126778b7456e
Hauke Wesselmann <hauke@h-dawg.de>
2021-04-01 18:19:05 +0200

    add initial endpoint without functionality for listings

--- src/main/java/de/hdawg/tennis/rankinginfo/controller/RankingController.java
+++ src/main/java/de/hdawg/tennis/rankinginfo/controller/RankingController.java
@@ -2,8 +2,15 @@ package de.hdawg.tennis.rankinginfo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class RankingController {
+
+    @GetMapping("/listings")
+    public String getRankingList(Model model) {
+        return "listing";
+    }
 }
