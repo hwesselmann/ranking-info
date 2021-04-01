b32c157fc5497472916c030fa108b96fc4cfe01c
Hauke Wesselmann <hauke@h-dawg.de>
2021-04-01 19:29:57 +0200

    add JdbcTemplate for first version database access

--- src/main/java/de/hdawg/tennis/rankinginfo/repository/RankingRepository.java
+++ src/main/java/de/hdawg/tennis/rankinginfo/repository/RankingRepository.java
@@ -1,11 +1,20 @@
 package de.hdawg.tennis.rankinginfo.repository;
 
 import de.hdawg.tennis.rankinginfo.model.Ranking;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class RankingRepository {
+
+    private final JdbcTemplate jdbcTemplate;
 
     public List<Ranking> retrieveRankingListItems() {
         return Collections.emptyList();
