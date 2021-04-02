50aba6b2851964ad468b2c3d0f94b593c1a04135
Hauke Wesselmann <hauke@h-dawg.de>
2021-04-02 20:21:35 +0200

    add little test for federation longnames to avoid typos

---
+++ src/test/java/de/hdawg/tennis/rankinginfo/model/FederationTest.java
@@ -0,0 +1,29 @@
+package de.hdawg.tennis.rankinginfo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FederationTest {
+
+    @Test
+    public void testLongname() {
+        assertEquals("Baden", Federation.BAD.getLongname());
+        assertEquals("Berlin-Brandenburg", Federation.BB.getLongname());
+        assertEquals("Bayern", Federation.BTV.getLongname());
+        assertEquals("Hamburg", Federation.HAM.getLongname());
+        assertEquals("Hessen", Federation.HTV.getLongname());
+        assertEquals("Rheinland-Pfalz", Federation.RPF.getLongname());
+        assertEquals("Schleswig-Holstein", Federation.SLH.getLongname());
+        assertEquals("Saarland", Federation.STB.getLongname());
+        assertEquals("Sachsen", Federation.STV.getLongname());
+        assertEquals("Mecklenburg-Vorpommern", Federation.TMV.getLongname());
+        assertEquals("Niedersachsen-Bremen", Federation.TNB.getLongname());
+        assertEquals("Sachsen-Anhalt", Federation.TSA.getLongname());
+        assertEquals("Thüringen", Federation.TTV.getLongname());
+        assertEquals("Mittelrhein", Federation.TVM.getLongname());
+        assertEquals("Niederrhein", Federation.TVN.getLongname());
+        assertEquals("Würtemberg", Federation.WTB.getLongname());
+        assertEquals("Westfalen", Federation.WTV.getLongname());
+    }
+}
