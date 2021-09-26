129478effd56f84f694bb919c96adb18360d0455
Hauke Wesselmann <hauke@h-dawg.de>
2021-09-26 19:51:59 +0200

    filter duplicates in player searches

--- app/models/player.rb
+++ app/models/player.rb
@@ -33,6 +33,93 @@ class Player
     clubs
   end
 
+  def self.find_all_players
+    players_to_filter = Ranking.select(:dtb_id, :lastname, :firstname, :federation, :club, :nationality)
+                               .order(:lastname, :dtb_id, date: :desc)
+                               .distinct
+    players = []
+    last_dtb_id = 0
+    players_to_filter.each do |player|
+      next if last_dtb_id.eql?(player.dtb_id)
+
+      players.push(player)
+      last_dtb_id = player.dtb_id
+    end
+    players
+  end
+
+  def self.find_players_by_lastname(lastname)
+    players_to_filter = Ranking.select(:dtb_id, :lastname, :firstname, :club, :federation, :nationality, :date)
+                               .where("LOWER(lastname) LIKE LOWER('%#{lastname}%')")
+                               .order(:lastname, :dtb_id, date: :desc)
+                               .distinct
+    players = []
+    last_dtb_id = 0
+    players_to_filter.each do |player|
+      next if last_dtb_id.eql?(player.dtb_id)
+
+      players.push(player)
+      last_dtb_id = player.dtb_id
+    end
+    players
+  end
+
+  def self.find_players_by_lastname_and_yob(lastname, yob_male, yob_female)
+    players_to_filter = Ranking.select(:dtb_id, :lastname, :firstname, :federation, :club, :nationality)
+                               .where("LOWER(lastname) LIKE LOWER('%#{lastname}%')
+                                       AND ((dtb_id >= #{yob_male * 100_000}
+                                       AND dtb_id <= #{yob_male * 100_000 + 99_999})
+                                       OR (dtb_id>= #{yob_female * 100_000}
+                                       AND dtb_id <= #{yob_female * 100_000 + 99_999}))")
+                               .order(:lastname, :dtb_id, date: :desc)
+                               .distinct
+    players = []
+    last_dtb_id = 0
+    players_to_filter.each do |player|
+      next if last_dtb_id.eql?(player.dtb_id)
+
+      players.push(player)
+      last_dtb_id = player.dtb_id
+    end
+    players
+  end
+
+  def self.find_players_by_dtb_id(dtb_id_start, dtb_id_end)
+    players_to_filter = Ranking.select(:dtb_id, :lastname, :firstname, :federation, :club, :nationality)
+                               .where("dtb_id >= #{dtb_id_start}
+                                       AND dtb_id <= #{dtb_id_end}")
+                               .order(:lastname, :dtb_id, date: :desc)
+                               .distinct
+    players = []
+    last_dtb_id = 0
+    players_to_filter.each do |player|
+      next if last_dtb_id.eql?(player.dtb_id)
+
+      players.push(player)
+      last_dtb_id = player.dtb_id
+    end
+    players
+  end
+  
+  def self.find_players_by_yob(yob_male, yob_female)
+    players_to_filter = Ranking.select(:dtb_id, :lastname, :firstname, :federation, :club, :nationality)
+                               .where("(dtb_id >= #{yob_male * 100_000}
+                                       AND dtb_id <= #{yob_male * 100_000 + 99_999})
+                                       OR (dtb_id >= #{yob_female * 100_000}
+                                       AND dtb_id <= #{yob_female * 100_000 + 99_999})")
+                               .order(:lastname, :dtb_id, date: :desc)
+                               .distinct
+    players = []
+    last_dtb_id = 0
+    players_to_filter.each do |player|
+      next if last_dtb_id.eql?(player.dtb_id)
+
+      players.push(player)
+      last_dtb_id = player.dtb_id
+    end
+    players
+  end
+
   attr_accessor :dtb_id, :current_ranking, :current_score, :lastname, :firstname
   attr_accessor :federation, :club, :nationality, :clubs, :rankings
 end
