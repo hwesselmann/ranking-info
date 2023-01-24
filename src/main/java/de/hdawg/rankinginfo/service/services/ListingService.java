8cf8a9d900e48ba3cf8630adf41379a27e5dd31a
Hauke Wesselmann <hauke@h-dawg.de>
2023-01-24 12:05:32 +0100

    map listings from db result

--- src/main/java/de/hdawg/rankinginfo/service/services/ListingService.java
+++ src/main/java/de/hdawg/rankinginfo/service/services/ListingService.java
@@ -2,12 +2,14 @@ package de.hdawg.rankinginfo.service.services;
 
 import de.hdawg.rankinginfo.service.model.Ranking;
 import de.hdawg.rankinginfo.service.model.listing.Listing;
+import de.hdawg.rankinginfo.service.model.listing.ListingItem;
 import de.hdawg.rankinginfo.service.repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,15 +22,15 @@ public class ListingService {
 
   private final RankingRepository rankingRepository;
 
-  public Listing getAgeGroupRankingsFilteredByClub(LocalDate quarter, String ageGroup, String gender, boolean isYobRanking, boolean overallRanking, boolean endOfYearRanking, String club) {
+  public Listing getAgeGroupRankingsFilteredByClub(LocalDate rankingPeriod, String ageGroup, String gender, boolean isYobRanking, boolean overallRanking, boolean endOfYearRanking, String club) {
     // TODO filter results for club
-    return getAgeGroupRankings(quarter, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
+    return getAgeGroupRankings(rankingPeriod, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
   }
 
   /**
    * retrieve the age group rankings from the datastore and map them to the json structure for the clients.
    *
-   * @param quarter ranking period
+   * @param rankingPeriod ranking period
    * @param ageGroup age group
    * @param gender gender
    * @param isYobRanking requested yob ranking only
@@ -36,17 +38,21 @@ public class ListingService {
    * @param endOfYearRanking requested end of year ranking
    * @return listing container
    */
-  public Listing getAgeGroupRankings(LocalDate quarter, String ageGroup, String gender, boolean isYobRanking, boolean overallRanking, boolean endOfYearRanking) {
+  public Listing getAgeGroupRankings(LocalDate rankingPeriod, String ageGroup, String gender, boolean isYobRanking, boolean overallRanking, boolean endOfYearRanking) {
     Listing result = Listing.builder()
-        .rankingPeriod(quarter)
+        .rankingPeriod(rankingPeriod)
         .ageGroup(ageGroup)
         .yobRanking(isYobRanking)
         .overallRanking(overallRanking)
         .endOfYearRanking(endOfYearRanking)
         .build();
-    // TODO map results from repository
-    List<Ranking> rankingsfromDb = rankingRepository.getRankingsForListing(quarter, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
-
+    List<Ranking> rankingsfromDb = rankingRepository.getRankingsForListing(rankingPeriod, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
+    List<ListingItem> listingItems = new ArrayList<>();
+    for (Ranking r : rankingsfromDb) {
+      ListingItem item = new ListingItem(r.position(), r.dtbId(), r.firstname(), r.lastname(), r.nationality(), r.club(), r.federation(), r.points());
+      listingItems.add(item);
+    }
+    result.setListingItems(listingItems);
     return result;
   }
 }
