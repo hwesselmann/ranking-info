cb6f7900d82e7dbe884eb19224c9aba5aacd7d8d
Hauke Wesselmann <hauke@h-dawg.de>
2023-03-17 18:35:03 +0100

    replace this loop with a stream

--- src/main/java/de/hdawg/rankinginfo/service/services/ListingService.java
+++ src/main/java/de/hdawg/rankinginfo/service/services/ListingService.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,13 +24,13 @@ public class ListingService {
   /**
    * retrieve the requested age groups. Filter results by given club String.
    *
-   * @param rankingPeriod ranking period
-   * @param ageGroup age group
-   * @param gender gender
-   * @param isYobRanking yob players only
-   * @param overallRanking overall ranking including younger players
+   * @param rankingPeriod    ranking period
+   * @param ageGroup         age group
+   * @param gender           gender
+   * @param isYobRanking     yob players only
+   * @param overallRanking   overall ranking including younger players
    * @param endOfYearRanking is end of year ranking
-   * @param club club to filter for
+   * @param club             club to filter for
    * @return listing container
    */
   public Listing getAgeGroupRankingsFilteredByClub(LocalDate rankingPeriod, String ageGroup, String gender, boolean isYobRanking, boolean overallRanking, boolean endOfYearRanking, String club) {
@@ -45,11 +44,11 @@ public class ListingService {
   /**
    * retrieve the age group rankings from the datastore and map them to the json structure for the clients.
    *
-   * @param rankingPeriod ranking period
-   * @param ageGroup age group
-   * @param gender gender
-   * @param isYobRanking requested yob ranking only
-   * @param overallRanking requested overall ranking including younger players
+   * @param rankingPeriod    ranking period
+   * @param ageGroup         age group
+   * @param gender           gender
+   * @param isYobRanking     requested yob ranking only
+   * @param overallRanking   requested overall ranking including younger players
    * @param endOfYearRanking requested end of year ranking
    * @return listing container
    */
@@ -62,12 +61,10 @@ public class ListingService {
         .overallRanking(overallRanking)
         .endOfYearRanking(endOfYearRanking)
         .build();
-    List<Ranking> rankingsfromDb = rankingRepository.getRankingsForListing(rankingPeriod, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
-    List<ListingItem> listingItems = new ArrayList<>();
-    for (Ranking r : rankingsfromDb) {
-      ListingItem item = new ListingItem(r.position(), r.dtbId(), r.firstname(), r.lastname(), r.nationality(), r.club(), r.federation(), r.points());
-      listingItems.add(item);
-    }
+    List<Ranking> rankingsFromDb = rankingRepository.getRankingsForListing(rankingPeriod, ageGroup, gender, isYobRanking, overallRanking, endOfYearRanking);
+    List<ListingItem> listingItems = rankingsFromDb.stream()
+        .map(r -> new ListingItem(r.position(), r.dtbId(), r.firstname(), r.lastname(), r.nationality(), r.club(), r.federation(), r.points()))
+        .toList();
     result.setListingItems(listingItems);
     return result;
   }
