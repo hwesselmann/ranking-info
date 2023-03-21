c0880596b123287c99fd5ac5835f5d5ed95a2b7f
Hauke Wesselmann <hauke@h-dawg.de>
2023-03-21 23:31:22 +0100

    add some more content to the player model

--- src/main/java/de/hdawg/rankinginfo/service/model/player/Player.java
+++ src/main/java/de/hdawg/rankinginfo/service/model/player/Player.java
@@ -4,6 +4,9 @@ import de.hdawg.rankinginfo.service.model.Federation;
 import de.hdawg.rankinginfo.service.model.Nationality;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
 @RequiredArgsConstructor
 public class Player {
   private final String dtbId;
@@ -13,4 +16,9 @@ public class Player {
 
   private final String currentClub;
   private final Federation currentFederation;
+
+  private HashMap<LocalDate, String> points;
+  private HashMap<LocalDate, HashMap<String, String>> rankingPositions;
+
+  private Trend trend;
 }
---
+++ src/main/java/de/hdawg/rankinginfo/service/model/player/Trend.java
@@ -0,0 +1,4 @@
+package de.hdawg.rankinginfo.service.model.player;
+
+public class Trend {
+}
