21b65a90f22c7d95743fb3e0c54524df16bceb7d
Hauke Wesselmann <hauke@h-dawg.de>
2023-03-29 10:52:31 +0200

    add trends block

---
+++ src/test/java/de/hdawg/rankinginfo/service/services/PlayerServiceTest.java
@@ -0,0 +1,29 @@
+package de.hdawg.rankinginfo.service.services;
+
+import de.hdawg.rankinginfo.service.repository.RankingRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerServiceTest {
+
+  @Mock
+  RankingRepository rankingRepository;
+
+  @DisplayName("eligible ageGroups are computed correctly")
+  @Test
+  void verifyEligibleAgeGroupsAreComputedCorrectly() {
+    PlayerService sut = new PlayerService(rankingRepository);
+    assertEquals(List.of("U17", "U18"), sut.getEligibleAgeGroups("10600000", LocalDate.of(2023, 4,1)));
+    assertEquals(List.of("U17", "U18"), sut.getEligibleAgeGroups("10600000", LocalDate.of(2023, 4,1)));
+    assertEquals(List.of("U11", "U12", "U13", "U14", "U15", "U16", "U17", "U18"),
+        sut.getEligibleAgeGroups("11300000", LocalDate.of(2024, 4,1)));
+    assertEquals(List.of("U15", "U16", "U17", "U18"),
+        sut.getEligibleAgeGroups("20800000", LocalDate.of(2023, 4,1)));
+  }
+}
